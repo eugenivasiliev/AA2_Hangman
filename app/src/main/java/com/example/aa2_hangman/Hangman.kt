adea05d8f2ef30d11e99b610d8c7a83df6644d22
eugenivasiliev <eugeni.vasiliev@enti.cat>
2025-11-06 17:28:35 +0100

    Full Hangman game working

--- app/src/main/java/com/example/aa2_hangman/Hangman.kt
+++ app/src/main/java/com/example/aa2_hangman/Hangman.kt
@@ -1,26 +1,27 @@
 package com.example.aa2_hangman
 
-import android.content.res.Resources
+import android.content.Intent
+import android.media.Image
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class Hangman : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_hangman)
 
-        val word: String? = intent.extras?.getString("word");
-        val text: TextView = findViewById(R.id.textView);
-        text.text = word;
+    lateinit var word_to_guess: String;
+    lateinit var guessedLetters: MutableSet<Char>;
+    lateinit var text: TextView;
+    lateinit var image: ImageView;
 
-        SetupKeyboard();
-    }
+    var imageIndex: Int = 0;
 
-    val keyIds: List<Int> = listOf(
+    val images: List<Int> = listOf<Int>(
+        R.drawable.ic_launcher_foreground,
+        R.drawable.ic_launcher_background
+    );
+
+    val keyIds: List<Int> = listOf<Int>(
         R.id.keyA, R.id.keyB, R.id.keyC, R.id.keyD, R.id.keyE,
         R.id.keyF, R.id.keyG, R.id.keyH, R.id.keyI, R.id.keyJ,
         R.id.keyK, R.id.keyL, R.id.keyM, R.id.keyN, R.id.keyO,
@@ -29,14 +30,66 @@ class Hangman : AppCompatActivity() {
         R.id.keyZ
     );
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hangman)
+
+        guessedLetters = mutableSetOf<Char>();
+        word_to_guess = GetGameWord();
+        text = findViewById<TextView>(R.id.text);
+        image = findViewById<ImageView>(R.id.image);
+
+        UpdateWord();
+        SetupKeyboard();
+
+    }
+
+    private fun GetGameWord(): String {
+        var str: String? = intent.extras?.getString("word");
+        str?.let { return str; }
+        throw Exception("Level word doesn't exist!");
+    }
+
+    private fun UpdateWord() {
+        var newText: String = "";
+        for(letter in word_to_guess) {
+            if(letter in guessedLetters) newText += letter;
+            else newText += '_';
+        }
+        if(newText == word_to_guess) Win();
+        else text.text = newText;
+    }
+
+    private fun UpdateImage(letter: Char) {
+        if(letter in word_to_guess) return;
+
+        imageIndex++;
+        if(imageIndex == images.size - 1) Lose();
+        else image.setImageResource(images[imageIndex]);
+
+    }
+
     private fun SetupKeyboard() {
         for (id in keyIds) {
-            val key: TextView = findViewById(id);
-            key.setOnClickListener { ProcessKey(key.text); }
+            val key: TextView = findViewById<TextView>(id);
+
+            //We pass in only the first letter since it's a single letter key
+            key.setOnClickListener { ProcessKey(key.text.first()); };
         }
     }
 
-    private fun ProcessKey(key: CharSequence) {
-        println(key);
+    private fun ProcessKey(letter: Char) {
+        guessedLetters.add(letter);
+        UpdateWord();
+        UpdateImage(letter);
+    }
+
+    private fun Win() {
+        startActivity(Intent(this, WinScreen::class.java));
+    }
+
+    private fun Lose() {
+        startActivity(Intent(this, LoseScreen::class.java));
     }
 }
